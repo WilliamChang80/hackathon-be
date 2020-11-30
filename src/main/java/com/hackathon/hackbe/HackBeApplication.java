1c793b6d92efbc15ff015795648c97fcbe1dfeae
WilliamChang80 <william_chang80@rocketmail.com>
2020-12-01 00:49:16 +0700

    Initial Commit

---
+++ src/main/java/com/hackathon/hackbe/HackBeApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.hackbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackBeApplication.class, args);
+	}
+
+}
